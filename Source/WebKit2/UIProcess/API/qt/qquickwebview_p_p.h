ec03e1b1f88ece0f9c27574a3876fa9cfb759cb0
Raine Makelainen <raine.makelainen@jollamobile.com>
2013-08-13 11:11:49 +0300

    [qtwebkit] Add APIs for delete cookies from qml
    This commit adds deleteAllCookies and deleteCookiesForHostname
    methods for experimental qml webview API.

--- Source/WebKit2/UIProcess/API/qt/qquickwebview_p_p.h
+++ Source/WebKit2/UIProcess/API/qt/qquickwebview_p_p.h
@@ -50,6 +50,7 @@ class QtWebContext;
 class QtWebError;
 class QtWebPageEventHandler;
 class QtWebPagePolicyClient;
+class WebCookieManagerProxy;
 class WebPageProxy;
 }
 
@@ -187,6 +188,7 @@ protected:
 
     QQuickWebViewPrivate(QQuickWebView* viewport);
     RefPtr<WebKit::WebPageProxy> webPageProxy;
+    RefPtr<WebKit::WebCookieManagerProxy> cookieManagerProxy;
     WKRetainPtr<WKPageRef> webPage;
     WKRetainPtr<WKPageGroupRef> pageGroup;
 
@@ -226,6 +228,7 @@ protected:
     bool m_renderToOffscreenBuffer;
     bool m_allowAnyHTTPSCertificateForLocalHost;
     bool m_autoCorrect;
+    bool m_temporaryCookies;
     QUrl m_iconUrl;
     int m_loadProgress;
     QString m_currentUrl;
